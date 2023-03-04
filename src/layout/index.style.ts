eeddfc98ada8797e82106330a02e602afaac08ed
supersmile0426 <akiranakao.yy@gmail.com>
2023-03-04 09:43:00 +0900

    update: change the width style

--- src/layout/index.style.ts
+++ src/layout/index.style.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 export const LayoutComponentStyle = styled('div')(({ theme }: any) => {
   return {
-    width: "99vw",
+    width: "100%",
     height: "100vh",
     display: "flex",
     flexDirection: "column",
